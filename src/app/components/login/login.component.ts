f9910c18f396bf74b4260d19043a1849c01447b8
Ghita <bahajghita3@gmail.com>
2024-02-08 14:20:40 +0100

    modify my routes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 // login.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/AuthService/auth.service';
 
 
 @Component({
@@ -8,6 +10,33 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  formLogin!: FormGroup;
+  errorMessage =undefined;
 
-}
+  constructor(private fb : FormBuilder, private router : Router, private authService : AuthService) {
+  }
+  ngOnInit() {
+    this.formLogin=this.fb.group({
+      email : this.fb.control(""),
+      password : this.fb.control("")
+    })
+  }
+
+  handleLogin() {
+
+    // if(this.formLogin.value.username=="admin" && this.formLogin.value.password=="1234"){
+    //   this.router.navigateByUrl("/students");
+    // }
+    let email=this.formLogin.value.email;
+    let password=this.formLogin.value.password;
+    this.authService.login(email, password)
+      .then(resp=>{
+        this.router.navigateByUrl("/dashboard");
+      })
+      .catch(error=>{
+        this.errorMessage=error;
+      })
+    }
+  
+  }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  fname: string = '';
+  lname: string = '';
+  email: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  handleRegister() {
+    const userData = {
+      fname: this.fname,
+      lname: this.lname,
+      email: this.email,
+      password: this.password
+    };
+
+   
+    this.http.post<any>('http://localhost:8080/api/v1/auth/register', userData).subscribe(
+      (response) => {
+      
+        this.router.navigateByUrl('/login');
+      },
+      (error) => {
+        console.error(error);
+     
+      }
+    );
+  }
 }
